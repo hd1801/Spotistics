2675fe87a0d058cafe910e1df19aa419b69f73bf
HD <hardikdhawan0108@gmail.com>
2022-03-28 02:09:25 +0530

    header links complete

--- client/src/Components/CustomHeader.jsx
+++ client/src/Components/CustomHeader.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { Header,Button,Title,Image, Group} from '@mantine/core';
+import { Header,Button,Title,Image, Group,UnstyledButton} from '@mantine/core';
 
 const CustomHeader = (props)=>{
+  const LinkStyle = {color:"#fff",fontSize:".9rem",fontWeight:"bolder"};
   return (
-    <Header className="shadow" height={60} padding="xl" style={{backgroundColor:"#070d17", border:"none"}} fixed>
+    <Header className="shadow" height={70} padding="md" style={{backgroundColor:"#070d17", border:"none"}} fixed>
     {/* Handle other responsive styles with MediaQuery component or createStyles function */}
-      <div style={{ display: 'flex',justifyContent: "space-between", justifyItems:"center",alignItems: 'center', height: '100%'  }}>
-        <Group spacing="xs" position="left">
+      <div style={{ display: 'flex',justifyContent: "space-between", alignItems: "center", height: '100%' ,flexWrap:"wrap" }}>
+        <Group spacing="xs" position="left" >
         <Image 
           width="1.8rem"
           src="/Spotify_Icon_RGB_Green.png"
@@ -15,7 +16,16 @@ const CustomHeader = (props)=>{
         style={{ letterSpacing: 2.5, color:"#fff", fontStyle:"bold"}}>
         SPOTISTICS</Title>
         </Group>
-        { !props.loginStatus && <Button radius="sm" color="green" size="sm" uppercase compact onClick={props.handleClick} >Login to spotify</Button>}
+       
+       { !props.loginStatus 
+          ? <Button radius="sm" color="green" size="sm" uppercase compact onClick={props.handleClick} >Login to spotify</Button>
+          : <Group spacing="sm" position="right" >
+              <UnstyledButton onClick={()=>props.changePage(0)} style={LinkStyle} >Home   </UnstyledButton>
+              <UnstyledButton onClick={()=>props.changePage(1)} style={LinkStyle} >Tracks  </UnstyledButton>
+              <UnstyledButton onClick={()=>props.changePage(2)} style={LinkStyle} >Artists   </UnstyledButton>
+            </Group>
+        }
+       
       </div>
       
     </Header>
