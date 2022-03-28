f9e12dc44b0b8525e868a866cc871a97372ea368
HD <hardikdhawan0108@gmail.com>
2022-03-28 20:21:17 +0530

    fixed useFetch for all types of api calls

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -4,7 +4,7 @@ import { TokenContext } from "../App";
 
 const useFetch= (URL)=>{
     const accessToken =  useContext(TokenContext);
-    const [data,dispatch] = useReducer(fetchReducer,initialState);
+    const [results,dispatch] = useReducer(fetchReducer,initialState);
     
     
     useEffect(() => {
@@ -13,11 +13,12 @@ const useFetch= (URL)=>{
         })
         .then(response => response.json())
         .then(data => {
-            dispatch({type: "FETCH_SUCCESS", payload: data.items });
+            
+            dispatch({type: "FETCH_SUCCESS", payload: ("items" in data? data.items: data) });
         })
         .catch(err => {dispatch({type: "FETCH_ERROR"});
         }
         )},[accessToken,URL]);  
-    return data;
+    return results;
 }
 export default useFetch;
