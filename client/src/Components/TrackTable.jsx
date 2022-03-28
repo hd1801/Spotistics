079f3276834c3bc50a557b1b9d98b25c4c55a1f1
HD <hardikdhawan0108@gmail.com>
2022-03-28 22:35:40 +0530

    added track table component

--- client/src/Components/TrackTable.jsx
+++ client/src/Components/TrackTable.jsx
@@ -4,20 +4,18 @@ const TrackTable = (props)=>{
         <tr>
         <th>Track</th>
         <th>Artist</th>
-        <th>Played At</th>
         </tr>
         )
         const getRows= (element)=>{
             return element.items.map((item,index)=>{
                  return(
                      <tr key={index}>
-                     <td>{item.track.name}</td>
-                     <td>{item.track.artists[0].name}</td>
-                     <td>{item.played_at}</td>
+                     <td>{"track" in item ? item.track.name : item.name}</td>
+                     <td>{"track" in item ? item.track.artists[0].name : item.artists[0].name }</td>
                      </tr>
                  )})
          }
-    return  <Card shadow="lg" p="sm" style={{backgroundColor:"#070d17"}} > 
+    return  <Card shadow="lg" p="sm" style={{backgroundColor:"#070d17",width:"55rem"}} > 
         <Title mb="1rem" order={3}>{props.Title}</Title>
         <ScrollArea style={{ height: 250 }} type="auto" scrollbarSize={12} scrollHideDelay={0}>
         <Table striped highlightOnHover>
