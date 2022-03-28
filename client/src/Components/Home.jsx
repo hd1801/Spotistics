53951bdce0444f3e5139dfe58268038b5a45603a
HD <hardikdhawan0108@gmail.com>
2022-03-28 22:36:09 +0530

    UI completed

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -1,18 +1,22 @@
 import { Container,Title,Group, Card, Text} from "@mantine/core";
 import TrackTable from "./TrackTable";
 import useFetch from "../hooks/useFetch";
-
+import ArtistTable from "./ArtistTable";
 
 
 const Home = () =>{
     const userURL = "https://api.spotify.com/v1/me"
     const recentURL = "https://api.spotify.com/v1/me/player/recently-played"
     const likedURL = "https://api.spotify.com/v1/me/tracks"
+    const timeRange = "long_term";
+    const artistURL = `https://api.spotify.com/v1/me/top/artists?time_range=${timeRange}`
+    const topTrackURL = `https://api.spotify.com/v1/me/top/tracks?time_range=${timeRange}`;
+    const topArtist = useFetch(artistURL)
     const likedTracks = useFetch(likedURL);
     const recentTracks  = useFetch(recentURL);
     const userData = useFetch(userURL);
-    console.log(userData);
-    console.log(likedTracks);
+    const topTracks = useFetch(topTrackURL);
+    console.log(topTracks);
 
     return <Container mt="4rem" p={0} mx={0} fluid>
             <Card shadow="lg" style={{backgroundColor:"#070d17"}}    >
@@ -21,12 +25,14 @@ const Home = () =>{
             
             </Card>
             
-            <Group mt="md" position="apart">
+            <Group mt="md" position="apart" grow>
             <TrackTable Title="Recently Played" Tracks={recentTracks}/>
             <TrackTable Title="Saved Tracks" Tracks={likedTracks}/>
             </Group>
-            
-        
+            <Group mt="md" position="apart" grow>
+            <ArtistTable Title="Top Artists" Artist={topArtist}/>
+            <TrackTable Title="Top Tracks" Tracks={topTracks}/>
+            </Group>
             
     </Container>
     
